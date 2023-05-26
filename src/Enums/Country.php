ebc0b825b6fede84db91fff1bfcfbc42e2451f50
Jesse Evers <jesse@jesseevers.com>
2023-05-26 01:36:23 -0400

    Move response header functionality to trait

--- src/Enums/Country.php
+++ src/Enums/Country.php
@@ -3,19 +3,9 @@
 namespace Walmart\Enums;
 
 // This is basically an enum, but since this library supports PHP 7.4 we can't use a literal enum
-final class Country
+final class Country extends AbstractEnum
 {
     public const CA = 'ca';
     public const MX = 'mx';
     public const US = 'us';
-
-    public static function all(): array
-    {
-        return [self::CA, self::MX, self::US];
-    }
-
-    public static function isValid(string $country): bool
-    {
-        return in_array($country, self::all(), true);
-    }
 }
